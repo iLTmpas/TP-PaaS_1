afc59d1295d537cb12837d8eb43c8e6da02d177f
assalay <126481767+assalay@users.noreply.github.com>
2025-11-17 17:45:51 +0100

    merge assala

--- worker-replication/src/main/java/fr/episen/worker/replication/repository/EmployeRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.episen.worker.replication.repository;
-
-import fr.episen.worker.replication.entity.EmployeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeRepository extends JpaRepository<EmployeEntity, Long> {
-}
